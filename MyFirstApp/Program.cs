17fb44bb8a5a80165b12b51cc1fa26984dd3fb94
Vladislav Podlegajev <vladislav.podlegajev@tktk.ee>
2024-04-03 01:43:43 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutaja sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+Console.WriteLine($"Hello, {userName} !");
+
